00b93836b305158b9accf730eeb74edc39b72353
Marcelo Bezerra <mmosca@github.com>
2026-01-12 21:37:42 +0100

    version

--- build_script.py
+++ build_script.py
@@ -1,4 +1,3 @@
 Import('env')
 
-env.Append(CPPDEFINES=["_GNU_SOURCE"])
-env.Append(CPPFLAGS=["-DGPIOD_VERSION_STR=\"\\\"2.2.2\\\"\""])
+env.Append(CPPDEFINES=["_GNU_SOURCE", ("GPIOD_VERSION_STR", "\"\\\"2.2.2\\\"\"")])
